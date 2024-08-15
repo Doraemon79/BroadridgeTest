0bf47422c1aa4e8cebc7fcad9724bfd035f36322
Doraemon79 <ignleotta@gmail.com>
2024-08-15 19:02:17 +0200

    added  readme

--- Broadridge/BroadRidgeTests/obj/Debug/net8.0/BroadRidgeTests.AssemblyInfo.cs
+++ Broadridge/BroadRidgeTests/obj/Debug/net8.0/BroadRidgeTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BroadRidgeTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19c82ab5f9f55aee2193af567432af11fa58655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de99108e4e961fdd8fe979ad0e8f618c5c1aee08")]
 [assembly: System.Reflection.AssemblyProductAttribute("BroadRidgeTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BroadRidgeTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
